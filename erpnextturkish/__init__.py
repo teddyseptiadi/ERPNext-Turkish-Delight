31216fec610eb0f14c33d8e54d3cc258f1d16499
Can Kılınç <walkingbee58@gmail.com>
2025-07-16 18:27:45 +0300

    taşıyıcı-irsaliye güncellemesi finalizer v6

--- erpnextturkish/__init__.py
+++ erpnextturkish/__init__.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 import frappe
 
-__version__ = '1.250711.5'
+__version__ = '1.250711.6'
 
 def console(*data):
 	frappe.publish_realtime('out_to_console', data, user=frappe.session.user)
